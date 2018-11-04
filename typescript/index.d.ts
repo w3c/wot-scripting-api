e1a345d7ed616444b58eb0481e74a1aedf31c222
Matthias Kovatsch <github@kovatsch.net>
2018-11-04 11:52:45 +0100

    typescript: update contenttype to contentType

--- typescript/index.d.ts
+++ typescript/index.d.ts
@@ -302,7 +302,7 @@ export interface Form {
     href: USVString;
     subprotocol?: USVString;
     op?: USVString | Array<USVString>;
-    contenttype?: USVString; // media type + parameter(s), e.g., text/plain;charset=utf8
+    contentType?: USVString; // media type + parameter(s), e.g., text/plain;charset=utf8
     security?: Security;
 }
 
