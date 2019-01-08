f039e55b71778fc05ab52d71383d8c3d15bef355
danielpeintner <daniel@peintner.org>
2019-01-08 09:04:58 +0100

    align with TD Security changes
    * SecurityScheme: rename proxyURI to proxy
    * Rename APIKeySecurityScheme to ApikeySecurityScheme
    * BearerSecurityScheme: add attribute authorization
    * CertSecurityScheme: missing
    * Rename PskScheme to PSKSecurityScheme and add attribute identity
    * PublicSecurityScheme: missing
    * Rename PopSecurityScheme to PoPSecurityScheme
    * OAuth2SecurityScheme: add attributes token and refresh

--- typescript/index.d.ts
+++ typescript/index.d.ts
@@ -351,13 +351,13 @@ export interface NullSchema extends BaseSchema {
     type: "null";
 }
 
-export type Security = NoSecurityScheme | BasicSecurityScheme | DigestSecurityScheme | BearerSecurityScheme | PopSecurityScheme |  ApikeySecurityScheme | OAuth2SecurityScheme | PskScheme;
+export type Security = NoSecurityScheme | BasicSecurityScheme | DigestSecurityScheme | BearerSecurityScheme | CertSecurityScheme | PoPSecurityScheme |  APIKeySecurityScheme | OAuth2SecurityScheme | PSKSecurityScheme | PublicSecurityScheme;
 
 
 export interface SecurityScheme {
     scheme: string;
     description?: string;
-    proxyURI?: any;
+    proxy?: string;
 }
 
 export interface NoSecurityScheme extends SecurityScheme {
@@ -372,41 +372,56 @@ export interface BasicSecurityScheme extends SecurityScheme {
 
 export interface DigestSecurityScheme extends SecurityScheme {
     scheme: "digest";
+    name?: string;
+    in?: string;
     qop?: string;
+}
+
+export interface APIKeySecurityScheme extends SecurityScheme {
+    scheme: "apikey";
     in?: string;
     name?: string;
 }
 
 export interface BearerSecurityScheme extends SecurityScheme {
     scheme: "bearer";
+    in?: string;
     alg?: string;
     format?: string;
-    in?: string;
     name?: string;
+    authorization?: string;
 }
 
-export interface PopSecurityScheme extends SecurityScheme {
+export interface CertSecurityScheme extends SecurityScheme {
+    scheme: "cert";
+    identity?: string;
+}
+
+export interface PSKSecurityScheme extends SecurityScheme {
+    scheme: "psk";
+    identity?: string;
+}
+
+export interface PublicSecurityScheme extends SecurityScheme {
+    scheme: "public";
+    identity?: string;
+}
+
+export interface PoPSecurityScheme extends SecurityScheme {
     scheme: "pop";
-    alg?: string;
     format?: string;
+    authorization?: string;
+    alg?: string;
+    name?: string;
     in?: string;
-    name?: string;
-}
-
-export interface ApikeySecurityScheme extends SecurityScheme {
-    scheme: "apikey";
-    in: string;
-    name?: string;
 }
 
 export interface OAuth2SecurityScheme extends SecurityScheme {
     scheme: "oauth2";
-    authorizationUrl: string;
+    authorization?: string;
+    flow?: string; // one of implicit, password, client, or code
+    token?: string;
+    refresh?: string;
     scopes?: Array<string>;
-    // one of implicit, password, client, or code
-    flow: string;
 }
 
-export interface PskScheme extends SecurityScheme {
-    scheme: "psk";
-}
